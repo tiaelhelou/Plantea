ed050c3ba4d461c899716c570185628068f9ac41
larissa.aboud <larissa.abboud@lau.edu>
2024-04-24 12:47:58 +0300

    create route groups

--- plantea_backend/routes/api.php
+++ plantea_backend/routes/api.php
@@ -17,3 +17,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'authentication'], function () {
+        Route::group([
+            'middleware' => 'api',
+            'prefix' => 'auth'], function () {
+
+            //api for admin auth
+        });
+    });
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+    
