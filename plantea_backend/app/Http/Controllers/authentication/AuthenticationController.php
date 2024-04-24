3135f0ae5b08e244dee9411115df90e6c861613f
tiahelou10 <tia.elhelou@outlook.com>
2024-04-24 13:42:49 +0300

    create authentication controller

---
+++ plantea_backend/app/Http/Controllers/authentication/AuthenticationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\authentication;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    //
+}
