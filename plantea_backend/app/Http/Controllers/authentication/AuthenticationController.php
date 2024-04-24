76f21168eb0632f42525a75361de0a74873c5ff7
tiahelou10 <tia.elhelou@outlook.com>
2024-04-25 00:38:36 +0300

    create register function

--- plantea_backend/app/Http/Controllers/authentication/AuthenticationController.php
+++ plantea_backend/app/Http/Controllers/authentication/AuthenticationController.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers\authentication;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+
+use Validator;
 
 class AuthenticationController extends Controller
 {
-    //
+    /*
+     * Register a User.
+     */
+    public function register(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 202);
+        }
+
+        $user = new User;
+
+        $user->user_name = $request->name;
+        $user->user_email = $request->email;
+        $user->user_password = bcrypt($request->password);
+    
+        if($user->save()){
+            return response()->json([
+                'success' => true,
+                'message' => 'User created successfully',
+                'data' => $user
+            ], 201);
+        }else{
+            return response()->json([
+                'success' => false,
+                'message' => 'User could not be created',
+            ], 200);
+        }
+
+    }
 }
