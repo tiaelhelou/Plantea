059f3b7345afa1ae1ea8853e705baf7a15a579ef
larissa.aboud <larissa.abboud@lau.edu>
2024-04-25 08:03:07 +0300

    create add plant function

--- plantea_backend/app/Http/Controllers/user/PlantCareController.php
+++ plantea_backend/app/Http/Controllers/user/PlantCareController.php
@@ -7,5 +7,42 @@ use Illuminate\Http\Request;
 
 class PlantCareController extends Controller
 {
-    //add plant , list plant reminder per plant
+    //add plant , list plant ,reminder per plant
+
+
+
+    public function addPlant(Request $request)
+    {
+        // add plant
+        $request->validate([
+            'user_id' => 'required',
+            'plant_id' => 'required',
+            'plant_nickname' => 'required',
+            'plant_health' => 'required'
+           
+        ]);
+        $plant = new Plant;
+        $plant->user_id = $request->user_id;
+        $plant->plant_id = $request->plant_id; //taken from a drop down list in app
+        $plant->plant_nickname = $request->plant_nickname;
+        $plant->plant_health = $request->plant_health;
+        //display plant info in this section (like plant library)
+        
+        
+        
+        if($plant->save()) {
+            return response()->json([
+                'result' => true,
+                'message' => 'plant added',
+                //'data' => $plant
+            ],200);
+        } else {
+            return response()->json([
+                'result' => false,
+                'message' => 'error',
+            ], 400);
+        }
+    }
+
+
 }
