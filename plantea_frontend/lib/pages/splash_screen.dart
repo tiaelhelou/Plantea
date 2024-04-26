e4bfc75ba40ee9397199ad727b0dd96fb4b8d12a
tiahelou10 <tia.elhelou@outlook.com>
2024-04-27 01:14:34 +0300

    add logo to splash

--- plantea_frontend/lib/pages/splash_screen.dart
+++ plantea_frontend/lib/pages/splash_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,10 +16,10 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
         body: Container(
           width: double.infinity,
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             color: Color.fromARGB(255, 153, 247, 171),
           ),
-          child: const Column(
+          child: Column(
             children: [
               SizedBox(height: 20),
               Text(
@@ -26,7 +29,18 @@ class _SplashScreenState extends State<SplashScreen> {
                   color: Color.fromARGB(255, 0, 50, 31),
                   fontSize: 96,
                 ),
-              )
+              ),
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    'assets/logo.jpg',
+                    width: 300,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
