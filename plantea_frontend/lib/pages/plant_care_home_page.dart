f75bb227a094c08583c2325ad59c0cb714bacbe1
larissa.aboud <larissa.abboud@lau.edu>
2024-04-29 10:39:24 +0300

    fix  list view logic

--- plantea_frontend/lib/pages/plant_care_home_page.dart
+++ plantea_frontend/lib/pages/plant_care_home_page.dart
@@ -30,6 +30,9 @@ class _PlantcareCopyWidgetState extends State<PlantcareWidget> {
     String ?selectedValue ;
 final TextEditingController textEditingController = TextEditingController();
 /// the list should be displayed in the listview and search bar
+
+
+/// FECTH DATA FROM DB 
 // Future<void> _fetchData() async {
 //     final response = await http.get(Uri.parse('http://your_backend_server_url/data'));
 //     if (response.statusCode == 200) {
@@ -159,94 +162,86 @@ containerList.add(Container(
 
                   ),
                 ),
-                Padding( //////drop down  in flutter
-                     padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-                      child: DropdownButtonHideUnderline(
-                        
-                      child: DropdownButton2<String>(
-                        
-                        isExpanded: true,
-                        hint: Text(
-                          'Select Item',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Theme.of(context).hintColor,
-                          ),
-                        ),
-                        items: list
-                            .map((item) => DropdownMenuItem(
-                                  value: item,
-                                  
-                                  child: Text(
-                                    item,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ))
-                            .toList(),
-                        value: selectedValue,
-                        onChanged: (value) {
-                          setState(() {
-                            selectedValue = value;
-                            
-                          });
-                        },
-                        buttonStyleData: const ButtonStyleData(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          height: 54,
-                          width: 296,
-                        ),
-                        dropdownStyleData: const DropdownStyleData(
-                          maxHeight: 200,
-                        ),
-                        menuItemStyleData: const MenuItemStyleData(
-                          height: 40,
-                        ),
-                        dropdownSearchData: DropdownSearchData(
-                          searchController: textEditingController,
-                          searchInnerWidgetHeight: 50,
-                          searchInnerWidget: Container(
-                            height: 50,
-                            padding: const EdgeInsets.only(
-                              top: 8,
-                              bottom: 4,
-                              right: 8,
-                              left: 8,
-                            ),
-                            child: TextFormField(
-                              expands: true,
-                              maxLines: null,
-                              controller: textEditingController,
-                              decoration: InputDecoration(
-                                isDense: true,
-                                contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 10,
-                                  vertical: 8,
-                                ),
-                                hintText: 'Search for an item...',
-                                hintStyle: const TextStyle(fontSize: 12),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                            ),
-                          ),
-                          searchMatchFn: (item, searchValue) {
-                            return item.value.toString().contains(searchValue);
-                          },
-                        ),
-                        //This to clear the search value when you close the menu
-                        onMenuStateChange: (isOpen) {
-                          if (!isOpen) {
-                            textEditingController.clear();
-                          }
-                        },
-                        
-                      ),
-                      
-                    ),
-                    ),
+                Padding(
+  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+  child: DropdownButtonHideUnderline(
+    child: DropdownButton2<String>(
+      isExpanded: true,
+      hint: Text(
+        'Select Item',
+        style: TextStyle(
+          fontSize: 14,
+          color: Theme.of(context).hintColor,
+        ),
+      ),
+      items: list.map((item) => DropdownMenuItem(
+        value: item,
+        child: Text(
+          item,
+          style: const TextStyle(
+            fontSize: 14,
+          ),
+        ),
+      )).toList(),
+      value: selectedValue, //// selected container  
+      onChanged: (value) {
+        setState(() {
+          selectedValue = value;
+          textEditingController.text = value ?? '';
+        });
+      },
+      buttonStyleData: const ButtonStyleData(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        height: 54,
+        width: 296,
+      ),
+      dropdownStyleData: const DropdownStyleData(
+        maxHeight: 200,
+      ),
+      menuItemStyleData: const MenuItemStyleData(
+        height: 40,
+      ),
+      dropdownSearchData: DropdownSearchData(
+        searchInnerWidgetHeight: 50,
+        searchInnerWidget: Container(
+          height: 50,
+          padding: const EdgeInsets.only(
+            top: 8,
+            bottom: 4,
+            right: 8,
+            left: 8,
+          ),
+          child: TextFormField(
+            expands: true,
+            maxLines: null,
+            controller: textEditingController,
+            decoration: InputDecoration(
+              isDense: true,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 8,
+              ),
+              hintText: 'Search for an item...',
+              hintStyle: const TextStyle(fontSize: 12),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ),
+        searchMatchFn: (item, searchValue) {
+          return item.value.toString().contains(searchValue);
+        },
+      ),
+      // This to clear the search value when you close the menu
+      onMenuStateChange: (isOpen) {
+        if (!isOpen) {
+          textEditingController.clear();
+        }
+      },
+    ),
+  ),
+),
                 Align(
                   alignment: AlignmentDirectional(-1, 0),
                   child: Padding(
@@ -270,7 +265,7 @@ Container(
     color: FlutterFlowTheme.of(context).secondaryBackground,
   ),
   child: 
-//list from database
+//FECTHED DATA WILL BE STORED IN A LIST 
 // ListView.builder(
 //         itemCount: _data.length,
 //         itemBuilder: (context, index) {
@@ -278,6 +273,50 @@ Container(
 //           return ListTile(
 //             title: Text(item['title']),
 //             subtitle: Text(item['description']),
+    //                 Padding( // list of container
+    //   padding: const EdgeInsets.symmetric(vertical: 10), // Add vertical padding
+    //   child: Container(
+    //     width: 296,
+    //     height: 150,
+    //     constraints: BoxConstraints(
+    //       minWidth: 296,
+    //       minHeight: 180,
+    //       maxWidth: 296,
+    //       maxHeight: 180,
+    //     ),
+    //     decoration: BoxDecoration(
+    //       color: FlutterFlowTheme.of(context).secondaryBackground,
+    //       image: DecorationImage(
+    //         fit: BoxFit.cover,
+    //         alignment: AlignmentDirectional(0, 0),
+    //         image: Image.network(
+    //           'https://images.unsplash.com/photo-1495231916356-a86217efff12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwxNXx8Zmxvd2VyfGVufDB8fHx8MTcxNDE0NjE1M3ww&ixlib=rb-4.0.3&q=80&w=400',
+    //         ).image,
+    //       ),
+    //       borderRadius: BorderRadius.circular(23),
+    //     ),
+    //     child: Align(
+    //       alignment: AlignmentDirectional(1, 1),
+    //       child: Padding(
+    //         padding: EdgeInsetsDirectional.fromSTEB(0, 0, 25, 25),
+    //         child: FlutterFlowIconButton(
+    //           borderRadius: 5,
+    //           borderWidth: 1,
+    //           buttonSize: 40,
+    //           fillColor: Color(0xFFFAF49D),
+    //           icon: Icon(
+    //             Icons.add_rounded,
+    //             color: Color(0xFF355E3B),
+    //             size: 24,
+    //           ),
+    //           onPressed: () {
+    //             print('IconButton pressed ...');
+    //           },
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
 //           );
 //         },
 //       ),
