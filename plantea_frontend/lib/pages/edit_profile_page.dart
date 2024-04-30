061ca9da739159ced46abfc801406190798aecab
tiahelou10 <tia.elhelou@outlook.com>
2024-04-30 16:30:40 +0300

    initialize classes

---
+++ plantea_frontend/lib/pages/edit_profile_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:plantea/pages/plant_Info.dart';
+import 'package:plantea/pages/profile_page.dart';
+import 'package:plantea/pages/settings_page.dart';
+import '../pages/plant_care_home_page.dart';
+import '../pages/welcome_page.dart';
+
+class EditProfileWidget extends StatefulWidget {
+  const EditProfileWidget({super.key});
+
+  @override
+  State<EditProfileWidget> createState() => _EditProfileWidgetState();
+}
+
+class _EditProfileWidgetState extends State<EditProfileWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
