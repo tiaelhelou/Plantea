3057c947dae822f662f5e8fbb49cef18dedbf8e9
tiahelou10 <tia.elhelou@outlook.com>
2024-04-30 15:34:31 +0300

    edit dispose function

--- plantea_frontend/lib/models/donate_model.dart
+++ plantea_frontend/lib/models/donate_model.dart
@@ -6,12 +6,27 @@ import '../pages/donate_page.dart';
 class DonateModel extends FlutterFlowModel<DonateWidget> {
   @override
   void dispose() {
-    // TODO: implement dispose
+    unfocusNode.dispose();
+    textFieldFocusNode1?.dispose();
+    textController1?.dispose();
+
+    textFieldFocusNode2?.dispose();
+    textController2?.dispose();
+
+    textFieldFocusNode3?.dispose();
+    textController3?.dispose();
+
+    textFieldFocusNode4?.dispose();
+    textController4?.dispose();
+
+    textFieldFocusNode5?.dispose();
+    textController5?.dispose();
+
+    textFieldFocusNode6?.dispose();
+    textController6?.dispose();
   }
 
   @override
-  void initState(BuildContext context) {
-    // TODO: implement initState
-  }
+  void initState(BuildContext context) {}
 
 }
