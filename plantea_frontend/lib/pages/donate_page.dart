9e204200bb2b070ef4981f4a89587d6246fb6202
tiahelou10 <tia.elhelou@outlook.com>
2024-04-30 15:13:34 +0300

    create donate page

---
+++ plantea_frontend/lib/pages/donate_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DonateWidget extends StatefulWidget {
+  const DonateWidget({super.key});
+
+  @override
+  State<DonateWidget> createState() => _DonateWidgetState();
+}
+
+class _DonateWidgetState extends State<DonateWidget> {
+   @override
+  Widget build(BuildContext context) {
+  }
+}
