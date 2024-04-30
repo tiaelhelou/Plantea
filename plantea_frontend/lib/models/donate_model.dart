0b19eba92743cf8ee6c6ed18c140409ceed75020
tiahelou10 <tia.elhelou@outlook.com>
2024-04-30 15:24:09 +0300

    add class donate model

---
+++ plantea_frontend/lib/models/donate_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+
+import '../pages/donate_page.dart';
+
+class DonateModel extends FlutterFlowModel<DonateWidget> {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void initState(BuildContext context) {
+    // TODO: implement initState
+  }
+
+}
