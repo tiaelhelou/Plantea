e67cb3e8a6ab15a451d39db17cffbabd8e352604
tiahelou10 <tia.elhelou@outlook.com>
2024-04-30 15:47:46 +0300

    add background

--- plantea_frontend/lib/pages/donate_page.dart
+++ plantea_frontend/lib/pages/donate_page.dart
@@ -85,6 +85,23 @@ class _DonateWidgetState extends State<DonateWidget> {
               topRight: Radius.circular(0),
             ),
           ),
+          alignment: AlignmentDirectional(0, 1),
+          child: Form(
+            key: _formKey,
+            child: Container(
+              width: 421,
+              height: 550,
+              decoration: BoxDecoration(
+                color: FlutterFlowTheme.of(context).secondaryBackground,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(0),
+                  bottomRight: Radius.circular(0),
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
