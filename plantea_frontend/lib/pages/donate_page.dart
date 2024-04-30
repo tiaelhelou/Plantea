b5553528fbf5607a831a9b2de0391e2a1c663065
tiahelou10 <tia.elhelou@outlook.com>
2024-04-30 15:40:04 +0300

    add initState()

--- plantea_frontend/lib/pages/donate_page.dart
+++ plantea_frontend/lib/pages/donate_page.dart
@@ -8,6 +8,31 @@ class DonateWidget extends StatefulWidget {
 }
 
 class _DonateWidgetState extends State<DonateWidget> {
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => DonateModel());
+
+    _model.textController1 ??= TextEditingController();
+    _model.textFieldFocusNode1 ??= FocusNode();
+
+    _model.textController2 ??= TextEditingController();
+    _model.textFieldFocusNode2 ??= FocusNode();
+
+    _model.textController3 ??= TextEditingController();
+    _model.textFieldFocusNode3 ??= FocusNode();
+
+    _model.textController4 ??= TextEditingController();
+    _model.textFieldFocusNode4 ??= FocusNode();
+
+    _model.textController5 ??= TextEditingController();
+    _model.textFieldFocusNode5 ??= FocusNode();
+
+    _model.textController6 ??= TextEditingController();
+    _model.textFieldFocusNode6 ??= FocusNode();
+  }
+  
    @override
   Widget build(BuildContext context) {
   }
