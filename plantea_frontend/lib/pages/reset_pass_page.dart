76afed3b469e3a57c1f4b194882eab7f3fccd113
tiahelou10 <tia.elhelou@outlook.com>
2024-05-02 19:16:38 +0300

    add class in reset page

---
+++ plantea_frontend/lib/pages/reset_pass_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import '../pages/plant_care_home_page.dart';
+import '../pages/welcome_page.dart';
+
+import '../models/reset_pass_model.dart';
+export '../models/reset_pass_model.dart';
+
+class ResetPassWidget extends StatefulWidget {
+  const ResetPassWidget({super.key});
+
+  @override
+  State<ResetPassWidget> createState() => _ResetPassWidgetState();
+}
+
+class _ResetPassWidgetState extends State<ResetPassWidget> {
+
+
+  @override
+  Widget build(BuildContext context) {
+  }
