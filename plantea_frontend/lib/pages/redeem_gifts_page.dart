5ebd84da88af06e3d405ea3cfe9e68a09be6f1e4
tiahelou10 <tia.elhelou@outlook.com>
2024-05-02 18:55:20 +0300

    add class in redeem page

---
+++ plantea_frontend/lib/pages/redeem_gifts_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:plantea/pages/add_plant_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import '../models/redeem_model.dart';
+export '../models/redeem_model.dart';
+
+
+class RedeemWidget extends StatefulWidget {
+  const RedeemWidget({Key? key}) : super(key: key);
+
+  @override
+  State<RedeemWidget> createState() => _RedeemWidgetState();
+}
+
+class _RedeemWidgetState extends State<RedeemWidget> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
