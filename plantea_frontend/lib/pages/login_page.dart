0b2bba516807bc17d00ec9f38e8c8db247e0c276
tiahelou10 <tia.elhelou@outlook.com>
2024-05-02 22:24:54 +0300

    fix login

--- plantea_frontend/lib/pages/login_page.dart
+++ plantea_frontend/lib/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:plantea/pages/profile_page.dart';
 import '../pages/plant_care_home_page.dart';
 import '../pages/welcome_page.dart';
 
@@ -221,7 +222,7 @@ class _LogWidgetState extends State<LogWidget> {
                                             context,
                                             MaterialPageRoute(
                                                 builder: (context) =>
-                                                    const WelcomeWidget()),
+                                                    const ProfileWidget()),
                                           );
                                         } else {
                                           ScaffoldMessenger.of(context)
