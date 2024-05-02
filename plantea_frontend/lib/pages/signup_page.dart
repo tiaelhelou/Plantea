e0176c5548430483108e48c23aa6cb9cff39e187
tiahelou10 <tia.elhelou@outlook.com>
2024-05-02 19:24:05 +0300

    create class in page

---
+++ plantea_frontend/lib/pages/signup_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:plantea/pages/add_plant_page.dart';
+import 'package:plantea/pages/welcome_page.dart';
+import '../models/signup_model.dart';
+export '../models/signup_model.dart';
+
+class SignupWidget extends StatefulWidget {
+  const SignupWidget({super.key});
+
+  @override
+  State<SignupWidget> createState() => _SignupWidgetState();
+}
+
+class _SignupWidgetState extends State<SignupWidget> {
+
+
+  @override
+  Widget build(BuildContext context) {
+  }
