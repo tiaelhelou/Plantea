1d70d42ac4dd832ca7e6440a11b8640bbb6d03b3
tiahelou10 <tia.elhelou@outlook.com>
2024-05-03 12:13:44 +0300

    edit reminder model

--- plantea_backend/app/Models/Reminder.php
+++ plantea_backend/app/Models/Reminder.php
@@ -1,11 +1,49 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Reminder
+ * 
+ * @property int $reminder_id
+ * @property string|null $reminder_type
+ * @property Carbon|null $reminder_time
+ * @property int $user_has_plants_user_id
+ * @property int $user_has_plants_plant_id
+ * 
+ * @property UserHasPlant $user_has_plant
+ *
+ * @package App\Models
+ */
 class Reminder extends Model
 {
-    use HasFactory;
+	protected $table = 'reminders';
+	public $incrementing = false;
+	public $timestamps = false;
+
+	protected $casts = [
+		'reminder_id' => 'int',
+		'reminder_time' => 'datetime',
+		'user_has_plants_user_id' => 'int',
+		'user_has_plants_plant_id' => 'int'
+	];
+
+	protected $fillable = [
+		'reminder_type',
+		'reminder_time'
+	];
+
+	public function user_has_plant()
+	{
+		return $this->belongsTo(UserHasPlant::class, 'user_has_plants_user_id')
+					->where('user_has_plants.user_id', '=', 'reminders.user_has_plants_user_id')
+					->where('user_has_plants.plant_id', '=', 'reminders.user_has_plants_plant_id');
+	}
 }
