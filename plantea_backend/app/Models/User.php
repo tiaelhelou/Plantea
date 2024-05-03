92dc2ec90b02cdf03cb4d29c4e29da2ad91ee3f1
tiahelou10 <tia.elhelou@outlook.com>
2024-05-03 12:33:36 +0300

    fix user model

--- plantea_backend/app/Models/User.php
+++ plantea_backend/app/Models/User.php
@@ -9,6 +9,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
 /**
  * Class User
  * 
@@ -29,10 +35,32 @@ use Illuminate\Database\Eloquent\Model;
  */
 class User extends Model
 {
+	use HasApiTokens, HasFactory, Notifiable;
+	
 	protected $table = 'users';
 	protected $primaryKey = 'user_id';
 	public $incrementing = false;
 	public $timestamps = false;
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    } 
 
 	protected $casts = [
 		'user_id' => 'int',
