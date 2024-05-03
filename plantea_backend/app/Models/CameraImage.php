57225cc06672e8d54fab5eb853e44ccadabe000e
tiahelou10 <tia.elhelou@outlook.com>
2024-05-03 12:11:24 +0300

    edit camera image model

--- plantea_backend/app/Models/CameraImage.php
+++ plantea_backend/app/Models/CameraImage.php
@@ -1,11 +1,41 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CameraImage extends Model
+/**
+ * Class Cameraimage
+ * 
+ * @property int $camera_image_id
+ * @property string|null $camera_image_image
+ * @property int $user_id
+ * 
+ * @property User $user
+ *
+ * @package App\Models
+ */
+class Cameraimage extends Model
 {
-    use HasFactory;
+	protected $table = 'cameraimages';
+	public $incrementing = false;
+	public $timestamps = false;
+
+	protected $casts = [
+		'camera_image_id' => 'int',
+		'user_id' => 'int'
+	];
+
+	protected $fillable = [
+		'camera_image_image'
+	];
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
 }
