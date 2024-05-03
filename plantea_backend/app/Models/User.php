c122af2b0339a812174e67fc60949123f2cdc540
tiahelou10 <tia.elhelou@outlook.com>
2024-05-03 12:12:34 +0300

    edit User model

--- plantea_backend/app/Models/User.php
+++ plantea_backend/app/Models/User.php
@@ -1,66 +1,79 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable implements JWTSubject
+/**
+ * Class User
+ * 
+ * @property int $user_id
+ * @property string|null $user_name
+ * @property string|null $user_email
+ * @property string|null $user_password
+ * @property string|null $user_profile
+ * @property int|null $user_points
+ * 
+ * @property Collection|Cameraimage[] $cameraimages
+ * @property Collection|Checkin[] $checkins
+ * @property Collection|Donate[] $donates
+ * @property Collection|Plant[] $plants
+ * @property Collection|Gift[] $gifts
+ *
+ * @package App\Models
+ */
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+	protected $table = 'users';
+	protected $primaryKey = 'user_id';
+	public $incrementing = false;
+	public $timestamps = false;
 
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
+	protected $casts = [
+		'user_id' => 'int',
+		'user_points' => 'int'
+	];
 
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
+	protected $hidden = [
+		'user_password'
+	];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	protected $fillable = [
+		'user_name',
+		'user_email',
+		'user_password',
+		'user_profile',
+		'user_points'
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	public function cameraimages()
+	{
+		return $this->hasMany(Cameraimage::class);
+	}
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+	public function checkins()
+	{
+		return $this->hasMany(Checkin::class);
+	}
+
+	public function donates()
+	{
+		return $this->hasMany(Donate::class);
+	}
+
+	public function plants()
+	{
+		return $this->belongsToMany(Plant::class, 'user_has_plants')
+					->withPivot('plant_nickname', 'plant_health');
+	}
+
+	public function gifts()
+	{
+		return $this->belongsToMany(Gift::class, 'user_redeems_gifts');
+	}
 }
