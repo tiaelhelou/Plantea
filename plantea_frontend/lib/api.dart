1acbb87476c67d27c7b2ebda83574ff9709d5b28
tiahelou10 <tia.elhelou@outlook.com>
2024-05-03 22:10:59 +0300

    add sign up api

--- plantea_frontend/lib/api.dart
+++ plantea_frontend/lib/api.dart
@@ -6,6 +6,39 @@ class Api {
   static String urlbase = 'http://127.0.0.1:8000/api/v1';
 
   /*
+   * Sign up api
+   */
+  static Future<bool> registerUser(
+      String name, String email, String password) async {
+    final url = Uri.parse('$urlbase/authentication/register');
+    final Map<String, dynamic> data = {
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+
+    try {
+      final response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        print('Registration failed with status: ${response.statusCode}');
+        return false;
+      }
+    } catch (error) {
+      print('Error registering user: $error');
+      return false;
+    }
+  }
+
+  /*
    * Login api
    */
   static Future<bool> loginUser(String email, String password) async {
@@ -53,5 +86,4 @@ class Api {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt('id', id);
   }
-  
 }
